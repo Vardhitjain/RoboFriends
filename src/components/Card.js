c41152b41b96f44f2ac6eee581f5c8894b0f07ad
Vardhitjain <vardhit.jain11@gmail.com>
2021-07-20 08:22:14 +0530

    adding Robo Friends

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const Card=(props)=>{
+	return(
+		<div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+			<img alt='robot' src={`https://robohash.org/${props.id}?200x200`}/>
+			<div>
+				<h2>{props.name}</h2>
+				<p>{props.email}</p>
+			</div>
+		</div>
+	);
+}
+export default Card;
